e34f585eda24302b474d3d95274b15b20ea1ff32
rctorr <rictor@cuhrt.com>
2018-11-02 06:42:34 -0600

    Se agrega primera versión del script genpass.py donde sólo se puede proporcionar la longitud de la clave

---
+++ Crea-tus-propios-comandos-en-Python/genpass-0.1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import click
+import random
+
+@click.command()
+@click.option("--len", default=8, type=int, help="Longitud de la clave")
+def genpass(len):
+    """
+    Genera una clave segura usando como base el alfabeto inglés en
+    mayúsculasx, minúsculas y digitos.
+    """
+    minusculas = "abcdefghijklmnopqrstuvwxyz"
+    mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digitos = "01234567890"
+
+    clave = random.choice(minusculas)
+    clave += random.choice(mayusculas)
+    clave += random.choice(digitos)
+
+    k = len - 3
+
+    faltan = random.choices(minusculas+mayusculas+digitos, k=k)
+
+    clave = clave.split() + faltan
+    random.shuffle(clave)
+    clave = "".join(clave)
+
+    print(clave)
+
+if __name__ == "__main__":
+    genpass()
