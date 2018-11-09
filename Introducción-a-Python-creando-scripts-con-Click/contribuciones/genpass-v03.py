7e20ba31f96074d227fcba19ea8ab62923e81676
rctorr <rictor@cuhrt.com>
2018-11-09 16:00:10 -0600

    Haciendo que la contribución de @petrohs genpass-v03.py funcione sin necesidad de pasar símbolos, también se cambian un poco los mensajes de ayuda del script

--- Introducción-a-Python-creando-scripts-con-Click/contribuciones/genpass-v03.py
+++ Introducción-a-Python-creando-scripts-con-Click/contribuciones/genpass-v03.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import click
 import random
 
 @click.command()
-@click.option("--len", default=8, type=int, help="longitud de la clave")
-@click.option("--num", default=1, type=int, help="cantidad de claves")
-@click.option("--sim", default="", type=str, help="caracteres")
+@click.option("--len", default=8, type=int, help="Longitud de la clave. Por omisión es 8.")
+@click.option("--num", default=1, type=int, help="Cantidad de claves. Por omisión es 1.")
+@click.option("--sim", default="", type=str, help="Símbolos a incluir en la clave. Ej: '!@#$%'")
 def genpass(len, num, sim):
     """
-    Generar una clave segura utilizando el alfanumerico ingles en mayusculas, minusculas y digitos
+    Generar una clave segura utilizando el alfanumerico inglés en mayúsculas, minúsculas y dígitos
     """
     minusculas="abcdefghijklmnopqrstuvwxyz"
     mayusculas="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     digitos="0123456789"
     
     for i in range(num):
-        clave =random.choice(minusculas)
-        clave+=random.choice(mayusculas)
-        clave+=random.choice(digitos)
-        clave+=random.choice(sim)
+        clave = random.choice(minusculas)
+        clave += random.choice(mayusculas)
+        clave += random.choice(digitos)
+        alfabeto = minusculas + mayusculas + digitos
         k=len-3
-        faltan=random.choices(minusculas+mayusculas+digitos, k=k)
-        clave=list(clave)+faltan
+        if sim:
+            clave += random.choice(sim)
+            alfabeto += sim
+            k -= 1
+        faltan = random.choices(alfabeto, k=k)
+        clave = list(clave) + faltan
         random.shuffle(clave)
-        clave="".join(clave)
+        clave = "".join(clave)
         print(clave)
 
 if __name__ == "__main__":
